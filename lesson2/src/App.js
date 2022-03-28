0bddc0327fba82537fbbb2a40eb7cfe9b26aa7a9
Nikolay <=>
2022-03-28 17:44:00 +0300

    less2

--- lesson2/src/App.js
+++ lesson2/src/App.js
@@ -1,19 +1,44 @@
 import React from "react";
 import './App.css';
 
-const User = (props) =>{
-  return <div>{props.name}</div>
+const User = (props) => {
+    return <div className="card">
+        <div>User Info:</div>
+        <div>Name: {props.name}</div>
+        <div>Surname: {props.surname}</div>
+        <div>Age: {props.age}</div>
+    </div>
+};
+
+const UserCardText = (props) => {
+  return <div className="info">My info: {props.name} {props.surname} {props.age}</div>
+};
+
+const Text = (props) => {
+  let text;
+  props.age >= 18 ? text = "могу": text = "не могу";
+
+  return <div className='text'>Привет {props.name}, тебе {props.age}, я {text} налить тебе выпить чего-нибудь</div>
 };
 
 class App extends React.Component {
-  state = {
-    name: "ds"
-  };
-  render() {
-    return <div>
-      <User name={this.props.name}/>
-    </div>
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "Igor",
+            surname: "Aleksandrov",
+            age: "22",
+        };
+    }
+
+    render() {
+        console.log(this.state);
+        return <div>
+            <User name={this.state.name} surName={this.state.surname} age={this.state.age}/>
+            <UserCardText name={this.state.name} surName={this.state.surname} age={this.state.age}/>
+            <Text name={this.state.name} surName={this.state.surname} age={this.state.age}/>
+        </div>
+    }
 }
 
 export default App;
---
+++ lesson2/src/Form.js
@@ -0,0 +1,54 @@
+import React from "react";
+import './Form.css';
+
+export default class Form extends React.Component {
+    state = {
+        name: "",
+        surname: "",
+        error:""
+    };
+
+    onSubmit = (event) =>{
+        event.preventDefault();
+        event.target.reset();
+        const reg = /[A-z]{3}/;
+        let name = this.state.name;
+        let surname = this.state.surname;
+
+        if(reg.test(name) && reg.test(surname)){
+            this.setState({name:'', surname:'', error: ''});
+            alert(` Добро пожаловать - ${name} ${surname}`)
+        }else {
+            this.setState({error : 'С твоими данными что то не так ;('});
+        }
+    };
+
+    changeValue = (event) =>{
+      this.setState({
+         [event.target.name]: event.target.value,
+      })
+    };
+
+    render() {
+        return <form onSubmit={this.onSubmit} className='form'>
+            <div>
+                <input
+                    placeholder='Name'
+                    name='name'
+                    value={this.state.name}
+                    onChange={this.changeValue}
+                />
+            </div>
+            <div>
+                <input
+                    placeholder='Surname'
+                    name='surname'
+                    value={this.state.surname}
+                    onChange={this.changeValue}
+                />
+            </div>
+            <button type='submit'>Submit</button>
+            <span>{this.state.error}</span>
+        </form>
+    }
+}
